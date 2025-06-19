64a1adb6d3624bcbb3b24be8b6705e7173ec26cb
JogiSudheerkumar <sudheerjogi123@gmail.com>
2025-06-19 23:40:14 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    items = ['T-Shirt', 'Jeans', 'Shoes']
+    return render_template('index.html', items=items)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
